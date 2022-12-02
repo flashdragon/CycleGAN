78e00678429949bee348b8e378c4460323918bf1
flashdragon <yooh3574@naver.com>
2022-12-03 00:48:00 +0900

    GAN추가

--- server/app.py
+++ server/app.py
@@ -1,24 +1,21 @@
 from flask import Flask, request, send_file
 from flask_cors import CORS
+from UGATIT import main
 from service import translate
 from werkzeug.utils import secure_filename
 import json
 
 app = Flask(__name__)
 CORS(app)
-@app.route('/')
-def wwww():
-
-    return "ww";
-
-
 
 
 @app.route('/', methods = ['POST'])
 def translateimage():
     f = request.files['file']
-    f.save("./image/"+secure_filename(f.filename))
-    filename='.\\image\\4844583611111.jpg'
+    print(f.filename)
+    f.save("./dataset/cat2emoticon/testA/"+secure_filename(f.filename))
+    main.main()
+    filename='./results/UGATIT_cat2emoticon_lsgan_4resblock_6dis_1_1_10_10_1000_sn_smoothing/'+secure_filename(f.filename)
     return send_file(filename,mimetype='image/jpg')
 
 
